f48e746a43d12ab49d543f56096de1ecd7565195
Aldrin Mathew <admin@aldrinmathew.com>
2022-07-22 14:33:05 +0530

    Added basic logging
    - Added logging functions for debug and release modes

---
+++ src/log.hpp
@@ -0,0 +1,21 @@
+#ifndef AXE_LOG_HPP
+#define AXE_LOG_HPP
+
+#include <iostream>
+
+#define LOG(val)                                                               \
+  std::cout << "\e[43m"                                                        \
+            << " AXE "                                                         \
+            << "\e[0;33m " << val << "\e[0m\n";
+
+#if IS_DEBUG_MODE
+#define DLOG(val)                                                              \
+  std::cout << "\e[44m"                                                        \
+            << " DEBUG "                                                       \
+            << "\e[0;34m " << val << "\e[0m"                                   \
+            << "\n";
+#else
+#define DLOG(val)
+#endif
+
+#endif
